1fccf4154fb3aed7811691e8a96fac9d5f6b199b
Belkacem Alidra <dev@b-alidra.com>
2018-08-15 02:33:57 +0200

    Fix program filters

--- front/src/js/app/Program/Filter.js
+++ front/src/js/app/Program/Filter.js
@@ -19,42 +19,48 @@ define(
     ['postal', 'jquery', 'bootstrap'],
     function dependencies(postal, $) {
 
-    function Filter($container, state) {
-        this.$container = $container;
+        function Filter($container, state) {
+            this.$container = $container;
 
-        this._class   = this.$container.data('class') || 'tkt_program_event';
-        this.$filters = $('.tkt-filter', this.$container);
-    }
-
-    Filter.prototype = {
-        attach: function() {
-            this.init();
-        },
-
-        init: function() {
-          this.$filters.click((e) => {
-              let $filter = $(e.target);
-              let type = $filter.data('type');
-
-              $('.tkt-filter').removeClass('active');
-              $filter.addClass('active');
-
-              this.filter_on($filter.data('type'));
-          });
-        },
-
-        filter_on: function (type) {
-            if (!type)
-                return $('.' + this._class + '[data-type!="' + type + '"]').fadeIn();
-
-            $('.' + this._class + '[data-type!="' + type + '"]').hide();
-            $('.' + this._class + '[data-type="' + type + '"]').fadeIn();
-        },
-
-        detach: function() {
-
+            this._class   = this.$container.data('class') || 'tkt_program_event';
+            this.$filters = $('.tkt-filter', this.$container);
         }
-    };
 
-    return Filter;
-});
+        Filter.prototype = {
+            attach: function() {
+                this.init();
+            },
+
+            init: function() {
+                this.$filters.click((e) => {
+                    let $filter = $(e.target);
+                    let type    = $filter.data('type');
+
+                    $('.tkt-filter').removeClass('active');
+                    $filter.addClass('active');
+
+                    this.filter_on($filter.data('type'));
+                });
+
+                this.$filters.each((i, f) => {
+                    let type = $(f).data('type');
+                    if (!type)
+                        return;
+                });
+            },
+
+            filter_on: function (type) {
+                if (!type)
+                    return $('.' + this._class + '[data-type!="' + type + '"]').fadeIn();
+
+                $('.' + this._class + '[data-type!="' + type + '"]').hide();
+                $('.' + this._class + '[data-type="' + type + '"]').fadeIn();
+            },
+
+            detach: function() {
+
+            }
+        };
+
+        return Filter;
+    });
