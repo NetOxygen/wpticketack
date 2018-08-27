115b6c316b3b08c519e605967a092118be39e71a
Belkacem Alidra <dev@b-alidra.com>
2018-08-27 20:32:55 +0200

    Enhance newsletters

--- app/helpers/sync_helper.class.php
+++ app/helpers/sync_helper.class.php
@@ -10,7 +10,11 @@ class SyncHelper
 
         if (!empty($events)) {
             array_map(function ($e) {
-                wp_insert_post(static::event_to_post($e));
+                $post    = static::event_to_post($e);
+                $post_id = wp_insert_post($post);
+                if (count($e->posters) > 0) {
+                    static::save_event_image($e, $post_id);
+                }
             }, $events);
         }
     }
@@ -44,6 +48,42 @@ class SyncHelper
         }
 
         return $post;
+    }
+
+    protected static function save_event_image($event, $post_id)
+    {
+        if (count($event->posters) == 0) {
+            return false;
+        }
+
+        $poster     = $event->posters[0];
+        $url        = $poster['url'];
+        $basename   = basename($url);
+        $upload_dir = wp_upload_dir();
+        $dest_path  = $upload_dir['path'].'/'.$basename;
+        $dest_url   = $upload_dir['url'].'/'.$basename;
+        $mime_type  = 'image/'.pathinfo($dest_path)['extension'];
+
+        file_put_contents($dest_path, file_get_contents($url));
+
+        $attachment = [
+           'guid'           => $dest_url,
+           'post_mime_type' => $mime_type,
+           'post_title'     => $event->localized_title_or_original('fr'),
+           'post_content'   => 'Image principale',
+           'post_status'    => 'inherit'
+        ];
+
+        $image_id = wp_insert_attachment($attachment, $dest_path, $post_id);
+
+        // Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
+        require_once( ABSPATH . 'wp-admin/includes/image.php' );
+        // Generate the metadata for the attachment, and update the database record.
+        $attach_data = wp_generate_attachment_metadata($image_id, $dest_path);
+
+        wp_update_attachment_metadata($image_id, $attach_data);
+
+        add_post_meta($post_id, '_thumbnail_id', $image_id);
     }
 
     function get_id_from_guid($guid)
--- app/templates/event/newsletter_event.tpl.php
+++ app/templates/event/newsletter_event.tpl.php
@@ -10,13 +10,26 @@
 
 $e = $data->event;
 
-$date_title = date_to_min_s($e->start_at());
-if ($e->start_at()->format('Y-m-d') != $e->stop_at()->format('Y-m-d')) {
-    $dates = sprintf(
-        "Du %s au %s",
-        date_to_min_s($e->start_at()),
-        date_to_min_s($e->stop_at())
-    );
+$date_title = "";
+if (!empty($e->opaque('free_text_3'))) {
+    $date_title = strip_tags($e->opaque('free_text_3')['fr'], '<br/>');
+    if (strip_tags($date_title) == "") {
+        $date_title = "";
+    }
+}
+
+if (empty($date_title)) {
+    $date_title = implode('<br/>', array_unique(array_map(function ($s) {
+        return str_replace(':', 'H', date_and_time_to_min_s($s->start_at()));
+    }, $e->screenings())));
+}
+
+$description = "";
+if (!empty($e->opaque('description'))) {
+    $description = strip_tags($e->opaque('description')['fr'], '<br/>');
+    if (strip_tags($description) == "") {
+        $description = "";
+    }
 }
 
 $ids = array_map(function ($s) {
@@ -24,69 +37,31 @@ $ids = array_map(function ($s) {
 }, $e->screenings());
 
 $first_poster = $e->first_poster();
+$upload_dir   = wp_upload_dir();
+$poster_url   = $upload_dir['url'].'/'.basename($first_poster->url);
 ?>
-<div class="tkt_event">
 
-  <?php if (!empty($first_poster)) : ?>
-  <div class="row">
-    <div class="col">
-        <img class="d-block w-100" src="<?= $first_poster->url ?>" alt="<?= $e->localized_title_or_original('fr') ?>">
-    </div>
-  </div>
-  <br/>
-  <?php endif; ?>
-
-  <div class="row">
-    <div class="col-sm left-col text-left align-self-start">
-
-      <div class="row">
-        <div class="col">
-          <span class="screening-date"><?= $date_title ?></span>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col">
-          <span class="title active">
-            <?= $e->localized_title_or_original('fr') ?>
-          </span>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col">
-          <span class="genre">
-            <?= $e->opaque()['genre'] ?>
-          </span>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-sm right-col text-right align-self-start">
-      <ul class="infos">
-        <li class="website">Site officiel : www.vaiteani.fr</li>
-        <li class="facebook">Facebook</li>
-        <li class="twitter">Twitter</li>
-        <li class="instagram">Instagram</li>
-        <li class="credits">Crédit photo: Frank Loriou</li>
-      </ul>
-    </div>
-
-  </div>
-
-  <div class="row">
-    <div class="col">
-      <div class="synopsis">
-        <span class="title assertive">
-          <?= $e->localized_title_or_original('fr') ?>
-        </span>
-        <span class="text">
-          S'asseoir tranquillement, prendre le temps et se mettre au rythme des îles de Polynésie l'espace d'une soirée. Et tout ça en plein hiver. Voici l'expérience que nous vous proposons avec Vaiteani. Quelque part dans une vallée du Pacifique sud, nichée sous les fougères arborescentes et les bougainvilliers rubiconds, se cache une source magique. On raconte qu’elle serait descendue du ciel pour nous abreuver de son chant profond et nous prendre sous sa coupe, si d’aventure on s’approchait pour la goûter. Vaiteani, c’est elle, la ‘source céleste’ en polynésien.
-        </span>
-      </div>
-    </div>
-  </div>
-
+<h2>
+  <?= strtoupper($e->opaque()['genre']) ?>
+</h2>
+<h3>
+  <strong><?= strtoupper($date_title) ?></strong><p>&nbsp;</p>
+</h3>
+<?php if (!empty($e->opaque('free_text_1'))) : ?>
+<div>
+  <?= $e->opaque('free_text_1')['fr'] ?><p>&nbsp;</p>
 </div>
-<link rel='stylesheet' id='sage/main.css-css'  href='https://balidra-epicentre.neto2.net/wp-content/themes/epicentre/dist/styles/main.css' type='text/css' media='all' />
-<link rel='stylesheet' id='tkt-main-css-css'  href='https://balidra-epicentre.neto2.net/wp-content/plugins/wpticketack/front/build/styles/main.css?ver=4.9.7' type='text/css' media='all' />
+<?php endif; ?>
+<div>
+  <?= $description ?><p>&nbsp;</p>
+</div>
+<div>
+  <?= $e->opaque('free_text_2')['fr'] ?><p>&nbsp;</p>
+</div>
+<div>
+<a href="<?= event_book_url($e) ?>" alt="$e->localized_title_or_original('fr')">Billets</a>
+</div>
+<div>
+<a href="<?= event_details_url($e) ?>" alt="$e->localized_title_or_original('fr')">En savoir plus</a>
+</div>
+<!--more-->
