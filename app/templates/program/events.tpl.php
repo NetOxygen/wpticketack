1dbc57891ba1cec068c0af4d6cd20c2d3fd7ac7a
Belkacem Alidra <dev@b-alidra.com>
2018-08-29 11:47:54 +0200

    Fix no events comment

--- app/templates/program/events.tpl.php
+++ app/templates/program/events.tpl.php
@@ -12,7 +12,7 @@
 ?>
 <div id="tkt_program" data-component="Program/BookabilityState">
   <?php if (empty($data->events)) : ?>
-    <h3 class="no-event-title">Aucun événement à afficher</h3>
+    <h3 class="no-event-title">Aucun événement programmé actuellement, revenez nous visiter prochainement.</h3>
   <?php else: ?>
     <?php foreach ($data->events as $event) : ?>
     <div class="tkt_program_event" data-type="<?= $event->opaque('type') ?>">
