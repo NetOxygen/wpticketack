e6fa806d6c075a6cd3a079e7554d0e302d2011e5
Belkacem Alidra <dev@b-alidra.com>
2018-10-04 17:36:08 +0200

    Adapt filters

--- front/src/js/app/Program/Filter.js
+++ front/src/js/app/Program/Filter.js
@@ -6,6 +6,10 @@
  * <div
  *    <!-- Required -->
  *    data-component="Program/Filter"
+ *
+ *    <!-- Optional -->
+ *    data-class="tkt_program_event"
+ *    data-criteria="type"
  * >
  *   <ul>
  *     <li class="tkt-filter>...</div>
@@ -16,13 +20,14 @@
  * </div>
  */
 define(
-    ['postal', 'jquery', 'bootstrap'],
-    function dependencies(postal, $) {
+    ['postal', 'jquery', 'lodash', 'bootstrap'],
+    function dependencies(postal, $, _) {
 
         function Filter($container, state) {
             this.$container = $container;
 
-            this._class   = this.$container.data('class') || 'tkt_program_event';
+            this.target   = this.$container.data('target') || '.tkt_program_event';
+            this.criteria = this.$container.data('criteria') || 'type';
             this.$filters = $('.tkt-filter', this.$container);
         }
 
@@ -32,31 +37,36 @@ define(
             },
 
             init: function() {
-                this.$filters.click((e) => {
-                    let $filter = $(e.target);
-                    let type    = $filter.data('type');
-
-                    $('.tkt-filter').removeClass('active');
-                    $filter.addClass('active');
-
-                    this.filter_on($filter.data('type'));
-                });
+                this.$filters.click((e) => this.handle_click(e));
 
                 this.$filters.each((i, f) => {
-                    let type = $(f).data('type');
-                    if (!type)
+                    let value = $(f).data(this.criteria);
+                    if (!value)
                         return;
-                    if ($('.' + this._class + '[data-type="' + type + '"]').length == 0)
+                    if ($(this.target + '[data-' + this.criteria + '*="' + value + '"]').length == 0)
                         $(f).remove();
                 });
             },
 
-            filter_on: function (type) {
-                if (!type)
-                    return $('.' + this._class + '[data-type!="' + type + '"]').fadeIn();
+            handle_click: function (e) {
+                let $filter = $(e.target);
+                if ($filter.parent('.tkt-filter').length) {
+                    $filter = $filter.parent('.tkt-filter');
+                }
+                let value = $filter.data(this.criteria);
 
-                $('.' + this._class + '[data-type!="' + type + '"]').hide();
-                $('.' + this._class + '[data-type="' + type + '"]').fadeIn();
+                $('.tkt-filter').removeClass('active');
+                $filter.addClass('active');
+
+                this.filter_on(this.target, this.criteria, value);
+            },
+
+            filter_on: (target, criteria, value) => {
+                if (!value)
+                    return $(this.target).not('[data-' + this.criteria + '*="' + value + '"]').fadeIn();
+
+                $(target).not('[data-' + criteria + '*="' + value + '"]').hide();
+                $(target + '[data-' + criteria + '*="' + value + '"]').fadeIn();
             },
 
             detach: function() {
