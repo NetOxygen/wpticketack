18566cece17f00ce78bd93559a2337e0a874731b
Belkacem Alidra <belkacem.alidra@netoxygen.ch>
2019-05-16 01:14:30 +0200

    Rework gallery template

--- app/templates/program/gallery/events.tpl.php
+++ app/templates/program/gallery/events.tpl.php
@@ -18,32 +18,34 @@ $item_width = $data->item_width;
 $nb_per_row = 12 / $item_width;
 ?>
 <div id="tkt_program" class="tkt-wrapper tkt-gallery" data-component="Program/BookabilityState">
-  <?php if (empty($data->events)) : ?>
-  <h3 class="no-event-title"><?= tkt_t('Aucun événement programmé actuellement, revenez nous visiter prochainement.') ?></h3>
-  <?php else: ?>
+    <div class="container">
+        <?php if (empty($data->events)) : ?>
+        <h3 class="no-event-title"><?= tkt_t('Aucun événement programmé actuellement, revenez nous visiter prochainement.') ?></h3>
+        <?php else: ?>
 
-    <?php $i = 0; ?>
-    <?php foreach($data->events as $event) : ?>
+          <?php $i = 0; ?>
+          <?php foreach($data->events as $event) : ?>
 
-        <?php if ($i % $nb_per_row == 0) : ?>
-        <div class="row">
+              <?php if ($i % $nb_per_row == 0) : ?>
+              <div class="row">
+              <?php endif; ?>
+
+                  <div class="tkt_program_event col col-<?= $item_width ?>" data-type="<?= $event->opaque('type') ?>">
+                      <?= TKTTemplate::render('program/gallery/event', (object)[ 'event' => $event ]) ?>
+                  </div>
+
+              <?php if ($i % $nb_per_row == ($nb_per_row - 1)) : ?>
+              </div>
+              <?php endif; ?>
+
+              <?php $i += 1; ?>
+
+          <?php endforeach; ?>
+
+          <?php if ($i % $nb_per_row != $nb_per_row) : ?>
+              </div>
+          <?php endif; ?>
+
         <?php endif; ?>
-
-            <div class="tkt_program_event col col-<?= $item_width ?>" data-type="<?= $event->opaque('type') ?>">
-                <?= TKTTemplate::render('program/gallery/event', (object)[ 'event' => $event ]) ?>
-            </div>
-
-        <?php if ($i % $nb_per_row == ($nb_per_row - 1)) : ?>
-        </div>
-        <?php endif; ?>
-
-        <?php $i += 1; ?>
-
-    <?php endforeach; ?>
-
-    <?php if ($i % $nb_per_row != $nb_per_row) : ?>
-        </div>
-    <?php endif; ?>
-
-  <?php endif; ?>
+    </div>
 </div>
