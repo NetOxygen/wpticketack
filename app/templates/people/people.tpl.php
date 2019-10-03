2c9959706e02c816301209d9ae19a2ca2d5f1e43
Alain Pellaux <alain.pellaux@netoxygen.ch>
2019-10-03 14:50:19 +0200

    Add reset filters

--- app/templates/people/people.tpl.php
+++ app/templates/people/people.tpl.php
@@ -24,7 +24,7 @@ use Ticketack\WP\Templates\TKTTemplate;
                         <?php $data->people->the_post(); ?>
                         <?php $meta = get_post_meta(get_the_ID()); ?>
 
-                        <div class="col-md-6 tkt-person" <?= tkt_person_data_attributes(get_post(), $data->filter_fields) ?>>
+                        <div class="col-md-6 tkt-person" style="display: none;" <?= tkt_person_data_attributes(get_post(), $data->filter_fields) ?>>
                             <?php echo TKTTemplate::render('people/person', $meta); ?>
                         </div>
 
@@ -33,23 +33,34 @@ use Ticketack\WP\Templates\TKTTemplate;
                 </div>
             </div>
             <div class="col-md-3">
+                <h4><?= tkt_t('Search') ?></h4>
+                <input type="text" class="tkt-filter-tags form-control" placeholder="<?= tkt_t('Search for') ?>">
                 <h4><?= tkt_t('Country') ?></h4>
                 <ul>
-                <?php foreach ($data->countries as $country) : ?>
-                    <li><a href="#country=<?= $country ?>"><?= $country ?></a></li>
-                <?php endforeach; ?>
+                    <li class="reset-filter"><a class="tkt-filter-country" href="#"><?= tkt_t('All') ?></a></li>
+                    <?php foreach ($data->countries as $country) : ?>
+                        <li style="display: none;">
+                            <a class="tkt-filter-country" href="#<?= $country ?>"><?= $country ?></a> (<span class="tkt-filter-country-total" data-country="<?= $country ?>">0</span>)
+                        </li>
+                    <?php endforeach; ?>
                 </ul>
                 <h4><?= tkt_t('Company') ?></h4>
                 <ul>
-                <?php foreach ($data->companies as $company) : ?>
-                    <li><a href="#company=<?= $company ?>"><?= $company ?></a></li>
-                <?php endforeach; ?>
+                    <li class="reset-filter"><a class="tkt-filter-company" href="#"><?= tkt_t('All') ?></a></li>
+                    <?php foreach ($data->companies as $company) : ?>
+                        <li style="display: none;">
+                            <a class="tkt-filter-company" href="#<?= $company ?>"><?= $company ?></a> (<span class="tkt-filter-company-total" data-company="<?= $company ?>">0</span>)
+                        </li>
+                    <?php endforeach; ?>
                 </ul>
                 <h4><?= tkt_t('Profession') ?></h4>
                 <ul>
-                <?php foreach ($data->professions as $profession) : ?>
-                    <li><a href="#profession=<?= $profession ?>"><?= $profession ?></a></li>
-                <?php endforeach; ?>
+                    <li class="reset-filter"><a class="tkt-filter-profession" href="#"><?= tkt_t('All') ?></a></li>
+                    <?php foreach ($data->professions as $profession) : ?>
+                        <li style="display: none;">
+                            <a class="tkt-filter-profession" href="#<?= $profession ?>"><?= $profession ?></a>  (<span class="tkt-filter-profession-total" data-profession="<?= $profession ?>">0</span>)
+                        </li>
+                    <?php endforeach; ?>
                 </ul>
             </div>
         </div>
