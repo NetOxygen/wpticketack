3a6d0bdfcfec626b67ff95a0f25d2f908e0b717f
Belkacem Alidra <belkacem.alidra@netoxygen.ch>
2020-05-13 14:24:24 +0200

    Release

--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Tags: ticketack
 Requires at least: 4.6
 Tested up to: 5.2
-Stable tag: 2.4.0
+Stable tag: 2.4.1
 Requires PHP: 5.6
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
@@ -34,6 +34,11 @@ This section describes how to install the plugin and get it working.
 == Screenshots ==
 
 == Changelog ==
+
+= 2.4.1 =
+* Get the infos for more than 100 screenings
+* Fix program shortcode day filter
+* Fix post images bug
 
 = 2.4.0 =
 * Handle theme provided shortcodes and JS components
--- wpticketack.php
+++ wpticketack.php
@@ -5,7 +5,7 @@
  * Description: Ticketack integration
  * Text Domain: wpticketack
  * Domain Path: /app/locales
- * Version: 2.4.0
+ * Version: 2.4.1
  * Author: Net Oxygen Sàrl
  * Author URI: https://netoxygen.ch
  * License: GPLv3
@@ -16,7 +16,7 @@ use Ticketack\WP\TKTApp;
 date_default_timezone_set(get_option('timezone_string'));
 setlocale(LC_TIME, get_locale().'.UTF-8');
 
-define('TKT_ASSETS_VERSION', '2.4.0.2019093001');
+define('TKT_ASSETS_VERSION', '2.4.1.2020051301');
 
 define("TKT_BASE", (dirname(__FILE__)));
 define("TKT_CONFIG", (TKT_BASE.'/config'));
