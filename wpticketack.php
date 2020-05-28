bfddac6b17df61e8fa5562673ead48d5e2b6dbff
Belkacem Alidra <belkacem.alidra@netoxygen.ch>
2020-05-28 14:07:01 +0200

    fix: Remove event_poster from bootstrap

--- wpticketack.php
+++ wpticketack.php
@@ -61,7 +61,6 @@ $app->register_action('Ticketack\WP\Actions\TranslationAction', 'translation.cla
 $app->register_action('Ticketack\WP\Actions\CustomTypesAction', 'custom_types.class.php');
 $app->register_shortcode('Ticketack\WP\Shortcodes\ProgramShortcode', 'program.class.php');
 $app->register_shortcode('Ticketack\WP\Shortcodes\EventShortcode', 'event.class.php');
-$app->register_shortcode('Ticketack\WP\Shortcodes\EventPosterShortcode', 'event_poster.class.php');
 $app->register_shortcode('Ticketack\WP\Shortcodes\NextScreeningShortcode', 'next_screening.class.php');
 $app->register_shortcode('Ticketack\WP\Shortcodes\ArticleShortcode', 'article.class.php');
 $app->register_shortcode('Ticketack\WP\Shortcodes\ShopShortcode', 'shop.class.php');
