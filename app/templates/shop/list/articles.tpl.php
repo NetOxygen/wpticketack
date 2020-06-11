b7d49001467beb2b096634484d8b455d36aeecfd
Alain Pellaux <alain.pellaux@netoxygen.ch>
2020-06-11 09:13:08 +0200

    ui: Add margin

--- app/templates/shop/list/articles.tpl.php
+++ app/templates/shop/list/articles.tpl.php
@@ -19,7 +19,7 @@ use Ticketack\WP\Templates\TKTTemplate;
         <h3 class="no-event-title"><?= tkt_t('Aucun article dans cette catégorie, revenez nous visiter prochainement.') ?></h3>
     <?php else: ?>
         <?php foreach ($data->articles as $chunk) : ?>
-            <div class="row">
+            <div class="row mb-4">
             <?php foreach ($chunk as $article) : ?>
                 <div class="col tkt-article">
                     <?= TKTTemplate::render('shop/list/article', (object)[ 'article' => $article ]) ?>
