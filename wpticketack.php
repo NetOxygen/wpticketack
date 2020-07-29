f679eb84c466e680836d1587779222b467ac669a
Belkacem Alidra <belkacem.alidra@netoxygen.ch>
2020-07-29 10:54:51 +0200

    fix: Fix plugin version

--- wpticketack.php
+++ wpticketack.php
@@ -5,7 +5,7 @@
  * Description: Ticketack integration
  * Text Domain: wpticketack
  * Domain Path: /app/locales
- * Version: 2.6.11
+ * Version: 2.7.0
  * Author: Net Oxygen Sàrl
  * Author URI: https://netoxygen.ch
  * License: GPLv3
