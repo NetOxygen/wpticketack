d6a148b0d4b7e695c8d7f1f8a6b3700a10bd0955
Belkacem Alidra <belkacem.alidra@netoxygen.ch>
2020-09-03 09:45:21 +0200

    Release

--- wpticketack.php
+++ wpticketack.php
@@ -5,7 +5,7 @@
  * Description: Ticketack integration
  * Text Domain: wpticketack
  * Domain Path: /app/locales
- * Version: 2.7.1
+ * Version: 2.7.2
  * Author: Net Oxygen Sàrl
  * Author URI: https://netoxygen.ch
  * License: GPLv3
@@ -16,7 +16,7 @@ use Ticketack\WP\TKTApp;
 date_default_timezone_set(get_option('timezone_string'));
 setlocale(LC_TIME, get_locale().'.UTF-8');
 
-define('TKT_ASSETS_VERSION', '2.7.1.2020090301');
+define('TKT_ASSETS_VERSION', '2.7.2.2020090301');
 
 define("TKT_BASE", (dirname(__FILE__)));
 define("TKT_CONFIG", (TKT_BASE.'/config'));
