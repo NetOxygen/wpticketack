cefce73d6786e36b39b551d7ce9241aeb8e2abd8
Belkacem Alidra <belkacem.alidra@netoxygen.ch>
2020-11-19 23:08:03 +0100

    i18n: Enhance translations

--- app/helpers/locales_helper.class.php
+++ app/helpers/locales_helper.class.php
@@ -11,14 +11,17 @@ class LocalesHelper
     {
         return [
             // Note: we need to write it this way so as xgettext can find our translations... ;(
-            'Veuillez choisir au moins un billet'                                                => tkt_t('Veuillez choisir au moins un billet'),
-            'Veuillez remplir les deux champs'                                                   => tkt_t('Veuillez remplir les deux champs'),
-            'Les informations que vous avez saisies sont invalides'                              => tkt_t('Les informations que vous avez saisies sont invalides'),
-            'Une erreur est survenue. Veuillez ré-essayer ultérieurement.'                       => tkt_t('Une erreur est survenue. Veuillez ré-essayer ultérieurement.'),
+            'Veuillez choisir au moins un billet'                                          => tkt_t('Veuillez choisir au moins un billet'),
+            'Veuillez remplir les deux champs'                                             => tkt_t('Veuillez remplir les deux champs'),
+            'Les informations que vous avez saisies sont invalides'                        => tkt_t('Les informations que vous avez saisies sont invalides'),
+            'Une erreur est survenue. Veuillez ré-essayer ultérieurement.'                 => tkt_t('Une erreur est survenue. Veuillez ré-essayer ultérieurement.'),
             'Vous ne pouvez pas réserver de place pour cette séance avec votre pass.'      => tkt_t('Vous ne pouvez pas réserver de place pour cette séance avec votre pass.'),
             'Vous ne pouvez pas réserver plus de place pour cette séance avec votre pass.' => tkt_t('Vous ne pouvez pas réserver plus de place pour cette séance avec votre pass.'),
-            'Veuillez choisir un tarif' => tkt_t('Veuillez choisir un tarif'),
-            'Votre panier a été mis à jour' => tkt_t('Votre panier a été mis à jour')
+            'Veuillez choisir un tarif'                                                    => tkt_t('Veuillez choisir un tarif'),
+            'Votre panier a été mis à jour'                                                => tkt_t('Votre panier a été mis à jour'),
+            'Code promo invalide'                                                          => tkt_t('Code promo invalide'),
+            'Impossible d\'utiliser ce code promo'                                         => tkt_t('Impossible d\'utiliser ce code promo'),
+            'Le code promo a bien été pris en compte'                                      => tkt_t('Le code promo a bien été pris en compte')
         ];
     }
 }
