ecef05376419647bb8b71b1bb5a40acbb9b69ee0
Belkacem Alidra <belkacem.alidra@netoxygen.ch>
2021-07-02 10:55:20 +0200

    fix: Fix Pantaflix url

--- app/templates/pantaflix/iframe.tpl.php
+++ app/templates/pantaflix/iframe.tpl.php
@@ -10,12 +10,14 @@ use Ticketack\WP\TKTApp;
  *   "ticket": Ticket
  *   "contentId": int
  * }
+ *
+
  */
 ?>
 <div style="overflow: hidden; padding-top: 56.25%; height: 0; position: relative; width: 100%;">
     <iframe
         style="border: 0; height: 100%; left: 0; position: absolute; top: 0; width: 100%;"
-        src="https://embed.cdn.pantaflix.com/latest?code=<%= ticket.id %>&provider=nifff#/nifff/playlist/<%= contentId %>"
+        src="https://embed.cdn.pantaflix.com/latest/?code=<%= ticket.id %>&provider=ticketack.nifff#/nifff/playlist/<%= contentId %>"
         allow="autoplay; encrypted-media; picture-in-picture"
         frameborder="0"
         allowfullscreen="true"
