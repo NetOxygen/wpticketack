6d8024927b034c5231e1a83cc65d865377834cb4
Jonathan Ernst <jonathan.ernst@netoxygen.ch>
2021-09-22 09:56:20 +0200

    fix: notices

--- app/filters/body_class.class.php
+++ app/filters/body_class.class.php
@@ -40,13 +40,14 @@ class BodyClassFilter extends TKTFilter
                 }
             }
         }
-
-        if ($post->post_type == 'tkt-event') {
-            $classes[] = 'body_tkt_event';
-        } elseif ($post->post_type == 'tkt-article') {
-            $classes[] = 'body_tkt_article';
-        } elseif ($post->post_type == 'tkt-people') {
-            $classes[] = 'body_tkt_people';
+        if (isset($post->post_type)) {
+            if ($post->post_type == 'tkt-event') {
+                $classes[] = 'body_tkt_event';
+            } elseif ($post->post_type == 'tkt-article') {
+                $classes[] = 'body_tkt_article';
+            } elseif ($post->post_type == 'tkt-people') {
+                $classes[] = 'body_tkt_people';
+            }
         }
 
         return $classes;
