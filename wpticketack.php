567e57c602538263cfbcad595364811d5bc79919
lcocquyt <ludovic.cocquyt@netoxygen.ch>
2022-03-08 11:16:06 +0100

    Release

--- wpticketack.php
+++ wpticketack.php
@@ -5,7 +5,7 @@
  * Description: Ticketack integration
  * Text Domain: wpticketack
  * Domain Path: /app/locales
- * Version: 2.30.4
+ * Version: 2.30.5
  * Author: Net Oxygen Sàrl
  * Author URI: https://netoxygen.ch
  * License: GPLv3
