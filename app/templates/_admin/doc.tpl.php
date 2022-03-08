c844d31c83931071f76e0f5c96a73ddd1f522f0e
lcocquyt <ludovic.cocquyt@netoxygen.ch>
2022-03-08 16:10:50 +0100

    Feat: Update the documentation link
    Test Plan: balidra jernst
    
    Reviewers: balidra
    
    Reviewed By: balidra
    
    Subscribers: balidra
    
    Differential Revision: https://phabricator.netoxygen.ch/D1640

--- app/templates/_admin/doc.tpl.php
+++ app/templates/_admin/doc.tpl.php
@@ -7,5 +7,5 @@ use Ticketack\WP\TKTApp;
  */
 ?>
 <h2>
-    <?= tkt_t('La documentation est disponible <a target="_blank" href="https://ticketack.com/documentation/Fran%C3%A7ais/Ticketack%20-%20Vente%20en%20ligne%20et%20Int%C3%A9gration%20billetterie.pdf">à cette adresse</a>'); ?>
+    <?= tkt_t('La documentation est disponible <a target="_blank" href="https://ticketack.com/knowledge-base/integration-avec-le-module-wordpress/">à cette adresse</a>'); ?>
 </h2>
