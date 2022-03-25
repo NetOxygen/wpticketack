dc95a7b443f7eab1935c7560fd5eec96494b611f
lcocquyt <ludovic.cocquyt@netoxygen.ch>
2022-03-25 15:18:54 +0100

    feat: Adding the knowledge base
    Test Plan: by hand
    
    Reviewers: jernst, balidra
    
    Differential Revision: https://phabricator.netoxygen.ch/D1650

--- app/templates/_admin/doc.tpl.php
+++ app/templates/_admin/doc.tpl.php
@@ -9,3 +9,19 @@ use Ticketack\WP\TKTApp;
 <h2>
     <?= tkt_t('La documentation est disponible <a target="_blank" href="https://ticketack.com/knowledge-base/integration-avec-le-module-wordpress/">à cette adresse</a>'); ?>
 </h2>
+<div id="ht-kb-fe-embed-container" class="Help ht-kb-fe-embed-container">
+    <div id="ht-kb-fe-embed-contents" style="display: block;">
+        <iframe id="ht-kb-fe-embed-iframe" src="https://ticketack.com/?kbembed=content" loading="lazy"></iframe>
+    </div>
+</div>
+<style type="text/css">
+    #ht-kb-fe-embed-iframe {
+        width: calc(100% - 20px);
+        height: 100%;
+        position: absolute;
+    }
+
+    div#wpfooter {
+        display: none;  
+    }
+</style>
