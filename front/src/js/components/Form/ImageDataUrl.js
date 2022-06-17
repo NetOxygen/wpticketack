403cc798b114cb8192c6e175827d8adb1aa827f3
lcocquyt <ludovic.cocquyt@netoxygen.ch>
2022-06-17 14:15:16 +0200

    feat: Display the loaded picture
    Summary: .
    
    Test Plan: .
    
    Reviewers: balidra, jernst, amonnin
    
    Reviewed By: balidra
    
    Differential Revision: https://phabricator.netoxygen.ch/D1707

--- front/src/js/components/Form/ImageDataUrl.js
+++ front/src/js/components/Form/ImageDataUrl.js
@@ -14,6 +14,7 @@ import { FileInputToDataUrl } from '../Ext';
 export default class ImageDataUrl extends Component {
     constructor($container, state) {
         super($container, state);
+        this.$parent = $container.parent();
     }
 
     attach() {
@@ -27,5 +28,25 @@ export default class ImageDataUrl extends Component {
             reset_on_submit: false
         });
         $('#photo_data_url').addClass('opaque_field');
+        this.load_preview();
+    }
+
+    load_preview() {
+        const readURL = input => {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = e => {
+                    if($('.image-preview', this.$parent))
+                        $('.image-preview', this.$parent).remove();
+
+                    $(input).parent().append('<img class="image-preview" src="' + e.target.result +'"></img>');
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        };
+
+        this.$container.change(() => {
+          readURL(this.$container[0]);
+        });
     }
 }
