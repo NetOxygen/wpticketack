bd0637f559d404d2cd85a7ff2d4d45f38fdcc1aa
lcocquyt <ludovic.cocquyt@netoxygen.ch>
2023-03-02 14:51:21 +0100

    Release

--- wpticketack.php
+++ wpticketack.php
@@ -5,7 +5,7 @@
  * Description: Ticketack integration
  * Text Domain: wpticketack
  * Domain Path: /app/locales
- * Version: 2.49.0
+ * Version: 2.49.1
  * Author: Net Oxygen Sàrl
  * Author URI: https://netoxygen.ch
  * License: GPLv3
@@ -15,7 +15,7 @@ use Ticketack\WP\TKTApp;
 
 setlocale(LC_TIME, get_locale().'.UTF-8');
 
-define('TKT_ASSETS_VERSION', '2.49.0.2023030203');
+define('TKT_ASSETS_VERSION', '2.49.1.2023030204');
 
 define("TKT_BASE", (dirname(__FILE__)));
 define("TKT_CONFIG", (TKT_BASE.'/config'));
