a76c9f3e7f8556e74409f7a3187f4cd3d474fc61
lcocquyt <ludovic.cocquyt@netoxygen.ch>
2024-02-26 11:53:35 +0100

    feat: Update template tkt_cart_summary
    Implement: F143

--- app/templates/cart/cart_summary_table.tpl.php
+++ app/templates/cart/cart_summary_table.tpl.php
@@ -8,13 +8,19 @@ use Ticketack\WP\TKTApp;
  *
  * Input: {
  *   "cart"              : Cart instance,
+ *   "theme"             : 'dark|light',
  *   "enable_promo_code" : bool
  * }
  */
 ?>
+<%
+    const fees = cart.getFees();
+%>
+
 <% if (cart.items.length > 0) { %>
+
 <div class="tkt-wrapper">
-    <section class="tkt-section tkt-light-section tkt-cart-section">
+    <section class="tkt-section tkt-<?= $data->theme ?>-section tkt-cart-section">
         <div class="row mt-3">
             <div class="col">
                 <h5> <?= tkt_t('Résumé du panier') ?></h5>
@@ -31,14 +37,21 @@ use Ticketack\WP\TKTApp;
                             <%= cart.getFormattedTotal(/*inversed*/true) %>
                         </td>
                     </tr>
-                    <tr>
-                        <td align="left">
-                            <?= tkt_t('Livraison') ?>
-                        </td>
-                        <td align="right">
-                            CHF 0.00
-                        </td>
-                    </tr>
+                    <% if (fees.length) { %>
+                        <tr>
+                            <td align="left">
+                                <?= tkt_t('Frais') ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <% _.each(fees, function(item) { %>
+                                <% if(item.type == "shipping" || item.type == "paymentfee") { %>
+                                    <td align="left" class="title-cell"><%= item.getFormattedTitle() %></td>
+                                    <td align="right" class="price-cell"><%= item.getFormattedPrice() %></td>
+                                <% } %>
+                            <% }); %>
+                        </tr>
+                    <% }; %>
                     <tr>
                         <td colspan="2">
                             <hr/>
