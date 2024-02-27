04f864bbe1cf6fc56affb59524c130bdacdaad22
Belkacem Alidra <belkacem.alidra@netoxygen.ch>
2024-02-27 19:21:53 +0100

    Revert "Revert "build: Remove PHP 7.4 support and remove notice on PHP 8.1""
    This reverts commit 3819e316a7c7793b9f8f33c4094e49ec25b72f30.

--- app/ticketack/models/place.class.php
+++ app/ticketack/models/place.class.php
@@ -154,11 +154,7 @@ class Place extends TKTModel implements \JsonSerializable
         return is_array($this->opaque);
     }
 
-    // XXX; once PHP 7.4 support is not needed anymore, remove the following
-    // line and add : mixed at the end of the signature so that PHP8.1 doesn't
-    // spew a deprecation warning.
-    #[ReturnTypeWillChange]
-    public function jsonSerialize()
+    public function jsonSerialize() : mixed
     {
         $ret = [
            'name'     => $this->name(),
