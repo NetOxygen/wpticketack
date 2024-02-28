554ece48721afa0be06de68126d7be4d63f9c78f
Jonathan Ernst <jonathan.ernst@netoxygen.ch>
2024-02-28 12:13:28 +0100

    i18n: Improve translations

--- app/templates/ticket/ticket.tpl.php
+++ app/templates/ticket/ticket.tpl.php
@@ -80,7 +80,7 @@
                                 <td>
                                     <% if (b.isCancelable) { %>
                                         <a href="#" class="btn btn-danger btn-bloc cancel-booking-btn" data-booking-id="<%= b._id %>">
-                                            <?= tkt_t("Annuler la réservation") ?>
+                                            <?= tkt_t("Annuler") ?>
                                         </a>
                                     <% } %>
                                 </td>
