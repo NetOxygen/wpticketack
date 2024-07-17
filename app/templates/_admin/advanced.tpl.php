72b038a679862ff0e7174f34658ca9d72a21764e
Belkacem Alidra <belkacem.alidra@netoxygen.ch>
2024-07-17 11:19:55 +0200

    deps: Update jscolor lib
    Linked to B2644

--- app/templates/_admin/advanced.tpl.php
+++ app/templates/_admin/advanced.tpl.php
@@ -14,5 +14,5 @@ $tab = 'advanced';
     do_settings_sections('ticketack-'.$tab);
     submit_button();
 ?>
-<script type="text/javascript" src="<?= plugin_dir_url(TKT_APP).'app/templates/_admin/js/jscolor.js' ?>" ></script>
+<script type="text/javascript" src="<?= plugin_dir_url(TKT_APP).'app/templates/_admin/js/jscolor.min.js' ?>" ></script>
 </form>
